fcd9e609cedbf35d34056002ceac822ccd3470d4
Tanja <Tanja@10.200.121.24>
2016-01-21 10:21:19 +0100

    GGWP det var det liv fuck og lort

---
+++ src/næstvedundgomsskole/Student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package næstvedundgomsskole;
+
+/**
+ *
+ * @author Tanja
+ */
+public class Student {
+    private String name;
+    private int telephone;
+    private int bYear;
+    private String school;
+    private String className;
+    private String password;
+    private String username;
+}
