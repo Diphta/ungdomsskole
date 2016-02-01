8380a2712ceb0bde1b742fdf2895121fbc1b4c1a
philip <philip@10.200.126.41>
2016-02-01 09:02:40 +0100

    dao

---
+++ dao/StudentDaoImpl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.ArrayList;
+import naestvedundgomsskole.Student;
+
+/**
+ *
+ * @author philip
+ */
+public class StudentDaoImpl {
+    ArrayList<Student> students;
+    
+    public StudentDaoImpl() {
+        students = new ArrayList<>();
+    }
+    
+    
+    public void deleteStudent(Student student){
+        
+    }
+    
+    public ArrayList<Student> getAllStudents() {
+        return students;
+    }
+    
+    public Student getStudent(int rollNo) {
+        return students.get(rollNo);
+    }
+    
+    public void updateStudent(Student student) {
+        //students.get(student.getRollNo().setName(student.getName());
+    }
+}
